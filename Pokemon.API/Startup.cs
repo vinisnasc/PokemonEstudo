d8ea6b3c97b7dfa83535cb54f620d9a980605ae1
Vinicius Nascimento <83601645+viniciusnasc@users.noreply.github.com>
2022-11-03 22:22:44 -0300

    Exemplo de uso de cors

--- Pokemon.API/Startup.cs
+++ Pokemon.API/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Net.Http.Headers;
 using Microsoft.OpenApi.Models;
 using PocketMonster.API.Configuration;
 using System;
@@ -33,6 +34,19 @@ namespace PocketMonster.API
             });
             services.AddIdentityConfig(Configuration);
             services.RegisterServices(Configuration);
+
+            /* Cors serve para facilitar a comunicação externa, caso o projeto seja aberto para outros sites, é necessario a implementação
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("Development", bdr => bdr.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod().AllowCredentials());
+                opt.AddPolicy("Production", bdr => bdr.WithMethods("GET")
+                                                      .WithOrigins("http://exemplo.com")
+                                                      .SetIsOriginAllowedToAllowWildcardSubdomains()
+                                                      .WithHeaders(HeaderNames.ContentType, "x-custom-header")
+                                                      .AllowAnyHeader());
+            }
+            );
+            */
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -42,6 +56,10 @@ namespace PocketMonster.API
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pokemon.API v1"));
+            }
+            else
+            {
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
